0f958c82e0f5b87c9384cf79e4ae89311cba0c97
Josh Saint Jacque <joshsaintjacque@gmail.com>
2024-10-13 01:24:41 -0700

    fix: Remove direct DOM manipulation (#4)
    This PR replaces direct DOM manipulation with React state updates.
    
    Directly manipulating the DOM in React is problematic because it
    bypasses React's virtual DOM, which is designed to efficiently manage
    UI updates.
    
    This can lead to inconsistencies between the actual DOM and the
    virtual DOM, making the application difficult to debug, hurting
    performance, and risking unpredictable behavior since React may not be
    aware of changes made outside its lifecycle.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Advocate } from "../db/schema";
 export default function Home() {
   const [advocates, setAdvocates] = useState<Advocate[]>([]);
   const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   useEffect(() => {
     console.log("fetching advocates...");
@@ -19,8 +20,7 @@ export default function Home() {
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = e.target.value.toLowerCase();
-
-    document.getElementById("search-term").innerHTML = searchTerm;
+    setSearchTerm(searchTerm);
 
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
@@ -40,6 +40,7 @@ export default function Home() {
   const onClick = () => {
     console.log(advocates);
     setFilteredAdvocates(advocates);
+    setSearchTerm("");
   };
 
   return (
@@ -50,9 +51,9 @@ export default function Home() {
       <div>
         <p>Search</p>
         <p>
-          Searching for: <span id="search-term"></span>
+          Searching for: <span>{searchTerm}</span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
+        <input style={{ border: "1px solid black" }} onChange={onChange} value={searchTerm} />
         <button onClick={onClick}>Reset Search</button>
       </div>
       <br />
