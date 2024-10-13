8f45f92dfb45423a2496a1fa24a7f5302de0dc15
Josh Saint Jacque <joshsaintjacque@gmail.com>
2024-10-13 01:05:52 -0700

    feature: Search improvements
    This PR extends the search functionality and fixes some bugs along the
    way. Search is now case-insensitive and supports partial matches in
    the specialties field.
    
    I've converted yearsOfExperience to a string since calling `includes`
    on the numeric field throws a runtime error.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,19 +18,19 @@ export default function Home() {
   }, []);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = e.target.value;
+    const searchTerm = e.target.value.toLowerCase();
 
     document.getElementById("search-term").innerHTML = searchTerm;
 
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
       return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
+        advocate.firstName.toLowerCase().includes(searchTerm) ||
+        advocate.lastName.toLowerCase().includes(searchTerm) ||
+        advocate.city.toLowerCase().includes(searchTerm) ||
+        advocate.degree.toLowerCase().includes(searchTerm) ||
+        advocate.specialties.some(specialty => specialty.toLowerCase().includes(searchTerm.toLowerCase())) ||
+        advocate.yearsOfExperience.toString().includes(searchTerm)
       );
     });
 
